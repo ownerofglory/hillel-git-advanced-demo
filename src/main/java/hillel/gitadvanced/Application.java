eac149233f8db3d4df142b1c8c25af7dcbc9c92c
ownerofglory <contact@ownerofglory.com>
2022-10-25 19:21:51 +0200

    Add output message
    add greeting output

--- src/main/java/hillel/gitadvanced/Application.java
+++ src/main/java/hillel/gitadvanced/Application.java
@@ -3,7 +3,7 @@ package hillel.gitadvanced;
 public class Application {
     public static void main(String[] args) {
         // comment
-
+        System.out.println("Hello World!");
         // issue-53 fix
     }
 }
