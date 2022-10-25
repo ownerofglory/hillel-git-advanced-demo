35a5e24169823db8e9f045c5bba4d9e940b3a110
ownerofglory <contact@ownerofglory.com>
2022-10-25 19:23:27 +0200

    Add greeting from Hillel

--- src/main/java/hillel/gitadvanced/Application.java
+++ src/main/java/hillel/gitadvanced/Application.java
@@ -3,7 +3,7 @@ package hillel.gitadvanced;
 public class Application {
     public static void main(String[] args) {
         // comment
-
+        System.out.println("Hello from Hillel");
         // issue-53 fix
     }
 }
